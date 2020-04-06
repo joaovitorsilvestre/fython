09da4a25519f82803eb4d567aa924a39a8ce361f
joao <joao_vitor_silvestre@outlook.com>
2020-04-06 20:01:24 -0300

    lists working

--- fython/before_ep6_improves/__init__.py
+++ fython/before_ep6_improves/__init__.py
@@ -113,6 +113,8 @@ TT_COMMA            = "COMMA"
 TT_DO               = "DO"
 TT_LPAREN           = "LPAREN"
 TT_RPAREN           = "RPAREN"
+TT_LSQUARE          = "LSQUARE"
+TT_RSQUARE          = "RSQUARE"
 TT_EOF              = 'EOF'
 
 KEYWORDS = [
@@ -189,6 +191,12 @@ class Lexer:
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '[':
+                tokens.append(Token(TT_LSQUARE, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == ']':
+                tokens.append(Token(TT_RSQUARE, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '!':
                 tok, error = self.make_not_equals()
@@ -535,6 +543,25 @@ class Number(Value):
         return str(self.value)
 
 
+class List(Value):
+  def __init__(self, elements):
+    super().__init__()
+    self.elements = elements
+
+  def added_to(self, other):
+    new_list = self.copy()
+    new_list.elements.append(other)
+    return new_list, None
+
+  def copy(self):
+    copy = List(self.elements[:])
+    copy.set_pos(self.pos_start, self.pos_end)
+    copy.set_context(self.context)
+    return copy
+
+  def __repr__(self):
+    return f'[{", ".join([str(x) for x in self.elements])}]'
+
 class Function(Value):
     def __init__(self, name, body_node, arg_names):
         super().__init__()
@@ -669,6 +696,19 @@ class Interpreter:
     def visit_StringNode(self, node, context):
         return RTResult().success(
             String(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+        )
+
+    def visit_ListNode(self, node, context):
+        res = RTResult()
+        elements = []
+
+        for element_node in node.element_nodes:
+            elements.append(res.register(self.visit(element_node, context)))
+            if res.error:
+                return res
+
+        return res.success(
+            List(elements).set_context(context).set_pos(node.pos_start, node.pos_end)
         )
 
     def visit_VarAccessNode(self, node, context):
@@ -844,6 +884,12 @@ class VarAssignNode:
         return f'(VAR_ASSIGN, ({self.var_name_tok}, {self.value_node})'
 
 
+class ListNode:
+    def __init__(self, element_nodes, pos_start, pos_end):
+        self.element_nodes = element_nodes
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+
 class BinOpNode:
     def __init__(self, left_node, op_tok, right_node):
         self.left_node = left_node
@@ -974,6 +1020,52 @@ class Parser:
 
         return res
 
+    def list_expr(self):
+        res = ParseResult()
+        element_nodes = []
+        pos_start = self.current_tok.pos_start.copy()
+
+        if self.current_tok.type != TT_LSQUARE:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                f"Expected '['"
+            ))
+
+        res.register_advancement()
+        self.advance()
+
+        if self.current_tok.type == TT_RSQUARE:
+            res.register_advancement()
+            self.advance()
+        else:
+            element_nodes.append(res.register(self.expr()))
+            if res.error:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    "Expected ']', 'VAR', 'IF', 'FOR', 'WHILE', 'FUN', "
+                    "int, float, identifier, '+', '-', '(', '[' or 'NOT'"
+                ))
+
+            while self.current_tok.type == TT_COMMA:
+                res.register_advancement()
+                self.advance()
+
+                element_nodes.append(res.register(self.expr()))
+                if res.error: return res
+
+            if self.current_tok.type != TT_RSQUARE:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    f"Expected ',' or ']'"
+                ))
+
+        res.register_advancement()
+        self.advance()
+
+        return res.success(ListNode(
+            element_nodes, pos_start, self.current_tok.pos_end.copy()
+        ))
+
     def if_expr(self):
         res = ParseResult()
         cases = []
@@ -1047,6 +1139,8 @@ class Parser:
                 res.register_advancement()
                 self.advance()
             else:
+                # TODO THIS IS LIKE THE ONE IN LIST_EXPR
+                # MAKE IT REUSABLE IN ONLY ONE FUNCTION
                 arg_nodes.append(res.register(self.expr()))
                 if res.error:
                     return res.failure(InvalidSyntaxError(
@@ -1107,6 +1201,12 @@ class Parser:
                     "Expected ')'"
                 ))
 
+        elif tok.type == TT_LSQUARE:
+            list_expr = res.register(self.list_expr())
+            if res.error:
+                return res
+            return res.success(list_expr)
+
         elif tok.matches(TT_KEYWORD, 'if'):
             if_expr = res.register(self.if_expr())
             if res.error:
@@ -1121,7 +1221,7 @@ class Parser:
 
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
-            "Expected int, float, identifier, '+', '-', '(', ')', if or def"
+            "Expected int, float, identifier, '+', '-', '(', '[', if or def"
         ))
 
     def power(self):
@@ -1163,7 +1263,7 @@ class Parser:
         if res.error:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Expected int, float, identifier, '+', '-', '(' or ')', 'not'"
+                "Expected int, float, identifier, '+', '-', '(' or '[', 'not'"
             ))
 
         return res.success(node)
@@ -1203,7 +1303,7 @@ class Parser:
         if res.error:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Expected VAR, int, float, identifier, '+', '-'or '('"
+                "Expected int, float, variable, 'not', '+', '-', '(' or '['"
             ))
 
         return res.success(node)
