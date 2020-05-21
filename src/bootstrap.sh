c0a41a36a563bfb54ea4cded87f6611d5ce8e143
joao <joao_vitor_silvestre@outlook.com>
2020-05-21 15:40:31 -0300

    sort the order of modules if is a bootstrap

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -3,3 +3,5 @@ mkdir _bootstrap
 
 # Compile all project as
 erl -pa ./_compiled -noshell -eval 'application:start(compiler),application:start(elixir),'"'"'Fcore.Generator.Compiler'"'"':compile_project("/home/joao/fython/src", "_bootstrap").' -s erlang halt
+
+erl -pa ./_bootstrap -noshell -eval 'application:start(compiler),application:start(elixir),'"'"'Fcore.Generator.Compiler'"'"':compile_project("/home/joao/fython/src", "_bootstrap2").' -s erlang halt
---
+++ src/builtins/map.fy
@@ -0,0 +1,5 @@
+def get(map, key):
+    get(map, key, None)
+
+def get(map, key, value):
+    Elixir.Map.get(map, key, value)
