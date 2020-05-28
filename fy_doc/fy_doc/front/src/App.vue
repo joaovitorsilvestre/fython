6d807db4a5ffa41aed8580e85122a003da615a4f
joao <joao_vitor_silvestre@outlook.com>
2020-05-28 00:04:32 -0300

    base front of fydocs

---
+++ fy_doc/fy_doc/front/src/App.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  #app
+    router-view
+</template>
+
+<script>
+export default {
+  methods: {
+    add () {
+      return ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
